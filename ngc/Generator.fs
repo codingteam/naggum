64ea7624481b63eef79fcd0addbff4481521686e
ForNeVeR <neverthness@gmail.com>
2011-12-10 16:15:04 +0700

    ngc: refactored Context usage.

--- ngc/Generator.fs
+++ ngc/Generator.fs
@@ -21,112 +21,67 @@ THE SOFTWARE. *)
 module Naggum.Compiler.Generator
 
 open System
+open System.Collections.Generic
 open System.Reflection
 open System.Reflection.Emit
 
-open Naggum.Context
 open Naggum.Reader
 open Naggum.Runtime
 open Naggum.Types
 
-/// Returns local variable for Context object.
+type private Context = Dictionary<string, MethodInfo>
+
 let private prologue (ilGen : ILGenerator) =
-    let listGetter = typeof<List<string * ContextItem>>.GetMethod "get_Empty"
-    let contextConstructor = typeof<Context>.GetConstructor [| typeof<List<string * ContextItem>> |]
-    let runtimeLoad = typeof<Runtime>.GetMethod "load"
-
     ilGen.BeginScope()
-    let contextVar = ilGen.DeclareLocal typeof<Context>
 
-    ilGen.Emit(OpCodes.Call, listGetter)
-    ilGen.Emit(OpCodes.Newobj, contextConstructor)
-    ilGen.Emit(OpCodes.Stloc, contextVar)
-    ilGen.Emit(OpCodes.Ldloc, contextVar.LocalIndex)
-    ilGen.Emit(OpCodes.Call, runtimeLoad)
+let private genApply (funcName : string) (context : Context) (ilGen : ILGenerator) : unit =
+    let func = context.[funcName]
+    ilGen.Emit(OpCodes.Call, func)
 
-    contextVar
-
-let private genApply (contextVar : LocalBuilder) (funcName : string) (ilGen : ILGenerator) : unit =
-    let newSymbol = typeof<Value>.GetMethod "NewSymbol"
-    let contextGet = typeof<Context>.GetMethod "get"
-    let valueGetter = typeof<ContextItem option>.GetMethod "get_Value"
-    let functionType = typeof<ContextItem>.GetNestedType "Function"
-    let functionItemGetter = functionType.GetMethod "get_Item"
-    let funcInvoke = typeof<SExp -> SExp>.GetMethod "Invoke"
-
-    ilGen.Emit(OpCodes.Ldloc, contextVar.LocalIndex)
-    ilGen.Emit(OpCodes.Ldstr, funcName)
-    ilGen.Emit(OpCodes.Call, newSymbol)
-    ilGen.Emit(OpCodes.Call, contextGet)
-    ilGen.Emit(OpCodes.Call, valueGetter)
-    ilGen.Emit(OpCodes.Castclass, functionType)
-    ilGen.Emit(OpCodes.Call, functionItemGetter)
-    ilGen.Emit(OpCodes.Call, funcInvoke)
-
-let private epilogue contextVar (ilGen : ILGenerator) =
+let private epilogue context (ilGen : ILGenerator) =
     let argGetter = typeof<Value>.GetMethod "get_EmptyList"
 
     ilGen.Emit(OpCodes.Call, argGetter)
-    genApply contextVar "main" ilGen
+    genApply "main" context ilGen
 
     ilGen.Emit OpCodes.Ret
     ilGen.EndScope()
 
-let rec private generate (typeBuilder : TypeBuilder) (ilGen : ILGenerator) (form : SExp) (contextVar : LocalBuilder) =
+let rec private generate (context : Context) (typeBuilder : TypeBuilder) (ilGen : ILGenerator) (form : SExp) =
     match form with
     | List list ->
         match list with
         | (Atom (Symbol "defun") :: Atom (Symbol name) :: List args :: body) ->
             let argsDef = Array.create (List.length args) typeof<obj>
             let methodGen = typeBuilder.DefineMethod(name, MethodAttributes.Public ||| MethodAttributes.Static, typeof<obj>, argsDef)
-            generateBody typeBuilder ilGen body contextVar
+            generateBody context typeBuilder ilGen body
 
             // Add function to context:
-            let newSymbol = typeof<Value>.GetMethod "NewSymbol"
-            let converterConstructor = typeof<Converter<SExp, SExp>>.GetConstructor [| typeof<obj>; typeof<nativeint> |]
-            let fSharpFuncFromConverter = typeof<SExp -> SExp>.GetMethod "FromConverter"
-            let newFunction = typeof<ContextItem>.GetMethod "NewFunction"
-            let contextAdd = typeof<Context>.GetMethod "add"
-
-            ilGen.Emit(OpCodes.Ldloc, contextVar.LocalIndex) // Context
-
-            ilGen.Emit(OpCodes.Ldstr, name)
-            ilGen.Emit(OpCodes.Call, newSymbol) // Context, Symbol
-
-            // Generate Converter instance:
-            ilGen.Emit(OpCodes.Ldnull) // Context, Symbol, null
-            ilGen.Emit(OpCodes.Ldftn, methodGen) // Context, Symbol, null, method
-            ilGen.Emit(OpCodes.Newobj, converterConstructor) // Context, Symbol, Converter
-
-            // Generate FSharpFunc:            
-            ilGen.Emit(OpCodes.Call, fSharpFuncFromConverter)
-            ilGen.Emit(OpCodes.Call, newFunction) // Context, Symbol, Function
-
-            ilGen.Emit(OpCodes.Call, contextAdd)
+            context.[name] <- methodGen
         | Atom (Symbol "if") :: condition :: if_true :: if_false :: [] ->
-            generate typeBuilder ilGen condition contextVar
+            generate context typeBuilder ilGen condition
             let if_true_lbl = ilGen.DefineLabel()
             let end_form = ilGen.DefineLabel()
             ilGen.Emit (OpCodes.Brtrue, if_true_lbl)
-            generate typeBuilder ilGen if_false contextVar
+            generate context typeBuilder ilGen if_false
             ilGen.Emit (OpCodes.Br,end_form)
             ilGen.MarkLabel if_true_lbl
-            generate typeBuilder ilGen if_true contextVar
+            generate context typeBuilder ilGen if_true
             ilGen.MarkLabel end_form
         | _ -> failwithf "%A not supported yet." list
     | other     -> failwithf "%A form not supported yet." other
-and private generateBody (typeBuilder : TypeBuilder) (ilGen : ILGenerator) (body : SExp list) (contextVar : LocalBuilder) =
+and private generateBody context (typeBuilder : TypeBuilder) (ilGen : ILGenerator) (body : SExp list) =
     match body with
     | [] ->
         let emptyListGetter = typeof<Value>.GetMethod "get_EmptyList"
         ilGen.Emit(OpCodes.Call, emptyListGetter)
         ilGen.Emit(OpCodes.Ret)
     | [last] ->
-        generate typeBuilder ilGen last contextVar
+        generate context typeBuilder ilGen last
         ilGen.Emit(OpCodes.Ret)
     | sexp :: rest ->
-        generate typeBuilder ilGen sexp contextVar
-        generateBody typeBuilder ilGen rest contextVar  
+        generate context typeBuilder ilGen sexp
+        generateBody context typeBuilder ilGen rest
 
 let compile (source : string) (assemblyName : string) (fileName : string) : unit =
     let assemblyName = new AssemblyName(assemblyName)
@@ -139,11 +94,12 @@ let compile (source : string) (assemblyName : string) (fileName : string) : unit
     
     let ilGenerator = methodBuilder.GetILGenerator()
 
-    let contextVar = prologue ilGenerator
+    let context = new Context()
+    prologue ilGenerator
     let sexp = Reader.parse source
-    generate typeBuilder ilGenerator sexp contextVar
+    generate context typeBuilder ilGenerator sexp
 
-    epilogue contextVar ilGenerator
+    epilogue context ilGenerator
 
     typeBuilder.CreateType()
     |> ignore

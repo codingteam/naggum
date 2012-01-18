bf95f7907018ffa02ed1ba74d8acf3ff12291ecf
hagane <deemson@gmail.com>
2012-01-18 16:47:51 +0500

    Symbol type

---
+++ Naggum.Runtime/Symbol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Naggum.Runtime
+{
+    class Symbol : IEquatable<Symbol>
+    {
+        public String Name { get; set; }
+        /// <summary>
+        /// Constructs new symbol object.
+        /// </summary>
+        /// <param name="aName">Symbol name</param>
+        public Symbol(String aName)
+        {
+            Name = aName;
+        }
+
+        bool IEquatable<Symbol>.Equals(Symbol other)
+        {
+            return Name.Equals(other.Name);
+        }
+    }
+}

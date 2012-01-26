471acf32376b5b422a9d58d50713ea39e5ae2700
hagane <deemson@gmail.com>
2012-01-26 14:44:22 +0500

    added command line key "r"

--- ngc/Program.fs
+++ ngc/Program.fs
@@ -24,8 +24,15 @@ open System
 open System.IO
 open Naggum.Compiler.Generator
 
-let fileName = Environment.GetCommandLineArgs().[1]
-let source = new StreamReader(File.Open (fileName,FileMode.Open))
-
-let assemblyName = Path.GetFileNameWithoutExtension fileName
-Generator.compile source assemblyName (assemblyName + ".exe") []
+let args = List.ofArray (Environment.GetCommandLineArgs())
+let mutable sources = []
+let mutable asmRefs = []
+for arg in (List.tail args) do
+    if arg.StartsWith "/r:" then
+        asmRefs <- arg.Replace("/r:","") :: asmRefs
+    else
+        sources <- arg :: sources
+for fileName in sources do
+    let source = new StreamReader(File.Open (fileName,FileMode.Open))
+    let assemblyName = Path.GetFileNameWithoutExtension fileName
+    Generator.compile source assemblyName (assemblyName + ".exe") asmRefs

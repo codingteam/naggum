2859c905a70c5d8e7a524100725f96dfa631816c
ForNeVeR <neverthness@gmail.com>
2012-10-23 21:42:02 +0700

    Naggum.Test: our first test method.

---
+++ Naggum.Test/CompilerTest.fs
@@ -0,0 +1,17 @@
+﻿namespace Naggum.Test
+open Naggum.Compiler
+open NUnit.Framework
+open System.Diagnostics
+open System.IO
+
+[<TestFixture>]
+type CompilerTest() =
+    let sourceFilename = @"..\..\..\tests\test.naggum"
+
+    [<Test>]
+    member this.RunTest() =
+        let filename = "test.exe"
+
+        use stream = File.Open(sourceFilename, FileMode.Open)
+        Generator.compile stream "test" filename []
+        ignore <| (Process.Start filename).WaitForExit(30000) // 30 sec should be enough

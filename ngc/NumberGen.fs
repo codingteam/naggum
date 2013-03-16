4530b50a24a33427ce2fd1a3a2b0db12c59b69f4
ForNeVeR <neverthness@gmail.com>
2013-03-16 12:26:29 +0700

    Compile-time type checking.

--- ngc/NumberGen.fs
+++ ngc/NumberGen.fs
@@ -24,27 +24,27 @@ open System
 open System.Reflection
 open System.Reflection.Emit
 
-type NumberGen<'TNumber>() =
+type NumberGen<'TNumber>(number: 'TNumber) =
     interface IGenerator with
         member this.Generate _ = failwith "Failure: Tried to generate unreified number constant.\n"
         member this.ReturnTypes () = [typeof<'TNumber>]        
 
 type Int32Gen(number: Int32) =
-    inherit NumberGen<Int32>()
+    inherit NumberGen<Int32>(number)
     interface IGenerator with
         member this.Generate ilGen = ilGen.Emit(OpCodes.Ldc_I4, number)
 
 type Int64Gen(number: Int64) =
-    inherit NumberGen<Int64>()
+    inherit NumberGen<Int64>(number)
     interface IGenerator with
         member this.Generate ilGen = ilGen.Emit(OpCodes.Ldc_I8, number)
 
 type SingleGen(number: Single) =
-    inherit NumberGen<Single>()
+    inherit NumberGen<Single>(number)
     interface IGenerator with
         member this.Generate ilGen = ilGen.Emit(OpCodes.Ldc_R4, number)
 
 type DoubleGen(number: Double) =
-    inherit NumberGen<Double>()
+    inherit NumberGen<Double>(number)
     interface IGenerator with
         member this.Generate ilGen = ilGen.Emit(OpCodes.Ldc_R8, number)

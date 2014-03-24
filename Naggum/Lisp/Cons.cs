caf86a8d2ba125301f2948f0e64c70d6ab3e68d4
ForNeVeR <neverthness@gmail.com>
2014-03-24 20:58:28 +0700

    Lisp: add some atom stuff (let's just count list as atom for simplicity).

---
+++ Naggum/Lisp/Cons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naggum.Lisp
+{
+	public class Cons : TypedAtom<Tuple<Atom, Atom>>
+	{
+		public Atom Car {
+			get { return TypedValue.Item1; }
+			set { TypedValue = Tuple.Create(value, TypedValue.Item2); } 
+		}
+
+		public Atom Cdr
+		{
+			get { return TypedValue.Item2; }
+			set { TypedValue = Tuple.Create(TypedValue.Item1, value); }
+		}
+	}
+}
---
+++ Naggum/Lisp/TypedAtom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naggum.Lisp
+{
+	public class TypedAtom<T> : Atom
+	{
+		public T TypedValue
+		{
+			get { return (T)Value; }
+			set { Value = value; }
+		}
+	}
+}

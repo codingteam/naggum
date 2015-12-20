fb9cbe2c481c8b882f178289e9878b4df9faab78
Friedrich von Never <friedrich@fornever.me>
2015-12-20 15:12:49 +0600

    Assembler: initial command line API.

---
+++ Naggum.Assembler/Assembler.fs
@@ -0,0 +1,15 @@
+﻿module Naggum.Assembler.Assembler
+
+open System.Reflection.Emit
+
+open Naggum.Compiler
+
+/// Prepares the source file for assembling. Returns the intermediate
+/// representation of the source code.
+let prepare fileName stream =
+    let forms = Reader.parse fileName stream
+    ()
+
+/// Assembles the source code. Returns a list of assemblies ready for saving.
+let assemble repr : AssemblyBuilder seq =
+    Seq.empty
--- Naggum.Assembler/Program.fs
+++ Naggum.Assembler/Program.fs
@@ -1,12 +1,52 @@
 ﻿module Naggum.Assembler.Program
 
+open System
+open System.IO
 open System.Reflection
+open System.Reflection.Emit
+
+type ReturnCode =
+    | Success = 0
+    | Error = 1
+    | InvalidArguments = 2
 
 let printUsage () =
     let version = Assembly.GetExecutingAssembly().GetName().Version
     printfn "Naggum Assembler %A" version
+    printfn "Usage: Naggum.Assembler [one or more file names]"
+
+let printError (error : Exception) =
+    printfn "Error: %s" (error.ToString ())
+
+let save (assembly : AssemblyBuilder) =
+    let name = assembly.FullName
+    assembly.Save name
+    printfn "Assembly %s saved" name
+
+let assemble fileName =
+    use stream = File.OpenRead fileName
+    let repr = Assembler.prepare fileName stream
+    let assemblies = Assembler.assemble repr
+    assemblies |> Seq.iter save
+
+let nga =
+    function 
+    | [| "--help" |] ->
+        printUsage ()
+        ReturnCode.Success
+    | fileNames when fileNames.Length > 0 ->
+        try
+            fileNames |> Array.iter assemble
+            ReturnCode.Success
+        with
+        | error ->
+            printError error
+            ReturnCode.Error
+    | _ ->
+        printUsage ()
+        ReturnCode.InvalidArguments
 
 [<EntryPoint>]
-let main _ = 
-    printUsage ()
-    0
+let main args =
+    let result = nga args
+    int result

6f60d36f7244fc09f3e6f88d2b165e05fd79cc2b
Friedrich von Never <friedrich@fornever.me>
2015-12-20 13:56:17 +0600

    Project: rename the assemblies, add Naggum.Assembler.

---
+++ Naggum.Assembler/Program.fs
@@ -0,0 +1,12 @@
+﻿module Naggum.Assembler.Program
+
+open System.Reflection
+
+let printUsage () =
+    let version = Assembly.GetExecutingAssembly().GetName().Version
+    printfn "Naggum Assembler %A" version
+
+[<EntryPoint>]
+let main _ = 
+    printUsage ()
+    0

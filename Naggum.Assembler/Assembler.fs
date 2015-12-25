58b159728373488e7b448ec07bd1854c5a7a8b47
Friedrich von Never <friedrich@fornever.me>
2015-12-25 23:27:02 +0600

    Assembler: fixup global methods.

--- Naggum.Assembler/Assembler.fs
+++ Naggum.Assembler/Assembler.fs
@@ -11,7 +11,7 @@ let private getMethodAttributes (m : MethodDefinition) =
     let conditions =
         [ (m.Visibility = Public, MethodAttributes.Public)
           (true, MethodAttributes.Static) ] // TODO: Proper static method detection
-    
+
     conditions
     |> List.map (fun (c, r) -> if c then r else empty)
     |> List.fold (|||) empty
@@ -23,7 +23,7 @@ let private findMethod (signature : MethodSignature) =
 
 let private buildMethodBody (m : MethodDefinition) (builder : MethodBuilder) =
     let generator = builder.GetILGenerator ()
-    
+
     m.Body
     |> List.iter (function
                   | Call signature ->
@@ -32,18 +32,20 @@ let private buildMethodBody (m : MethodDefinition) (builder : MethodBuilder) =
                   | Ldstr string -> generator.Emit (OpCodes.Ldstr, string)
                   | Ret -> generator.Emit (OpCodes.Ret))
 
-let private assembleUnit (builder : ModuleBuilder) = function
+let private assembleUnit (assemblyBuilder : AssemblyBuilder) (builder : ModuleBuilder) = function
     | Method m ->
         let name = m.Name
         let attributes = getMethodAttributes m
         let returnType = m.ReturnType
         let argumentTypes = Array.ofList m.ArgumentTypes
+        System.Diagnostics.Debug.Print (sprintf "%A" m)
         let methodBuilder = builder.DefineGlobalMethod (name,
                                                         attributes,
                                                         returnType,
                                                         argumentTypes)
+        if Set.contains EntryPoint m.Metadata then
+            assemblyBuilder.SetEntryPoint methodBuilder
         buildMethodBody m methodBuilder
-        
 
 let private assembleAssembly (assembly : Assembly) =
     let name = AssemblyName assembly.Name
@@ -52,7 +54,8 @@ let private assembleAssembly (assembly : Assembly) =
                                                 AssemblyBuilderAccess.Save)
     let fileName = assembly.Name + ".dll" // TODO: Proper file naming
     let moduleBuilder = builder.DefineDynamicModule (assembly.Name, fileName)
-    assembly.Units |> List.iter (assembleUnit moduleBuilder)
+    assembly.Units |> List.iter (assembleUnit builder moduleBuilder)
+    moduleBuilder.CreateGlobalFunctions ()
     builder
 
 /// Assembles the intermediate program representation. Returns a list of

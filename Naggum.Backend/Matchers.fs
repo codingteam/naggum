8c30a54fb91a3603babe411c639a491e1bfb640d
Friedrich von Never <friedrich@fornever.me>
2016-03-07 20:21:16 +0600

    #29: add a test and fix an issue shown on review

--- Naggum.Backend/Matchers.fs
+++ Naggum.Backend/Matchers.fs
@@ -1,7 +1,10 @@
 ﻿module Naggum.Backend.Matchers
 
-let (|String|Symbol|Object|List|) = function
-    | Reader.Atom (Reader.Object (:? string as s)) -> String s
+let (|Symbol|Object|List|) = function
     | Reader.Atom (Reader.Object o) -> Object o
     | Reader.Atom (Reader.Symbol x) -> Symbol x
     | Reader.List l -> List l
+
+let (|String|_|) = function
+    | Object (:? string as s) -> Some s
+    | _ -> None
